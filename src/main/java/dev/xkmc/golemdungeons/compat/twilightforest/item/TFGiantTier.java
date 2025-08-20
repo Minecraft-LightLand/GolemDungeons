e17401f3ae9c3f297b8c92c5edc07e29add7925a
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-08-20 11:31:58 +0800

    misc fix

--- src/main/java/dev/xkmc/golemdungeons/compat/twilightforest/item/TFGiantTier.java
+++ src/main/java/dev/xkmc/golemdungeons/compat/twilightforest/item/TFGiantTier.java
@@ -23,8 +23,8 @@ public class TFGiantTier {
 	public static final Tier KNIGHTMETAL;
 
 	static {
-		IRONWOOD = TierSortingRegistry.registerTier(new ForgeTier(2, 2048, 6.5F, 2.0F, 25, BlockTags.create(TwilightForestMod.prefix("needs_ironwood_tool")), () -> Ingredient.of(TwilightGDRegistry.GIANT_IRONWOOD_INGOT.get())), GolemDungeons.loc("ironwood"), List.of(Tiers.IRON), List.of(Tiers.DIAMOND));
+		IRONWOOD = TierSortingRegistry.registerTier(new ForgeTier(2, 4096, 6.5F, 2.0F, 25, BlockTags.create(TwilightForestMod.prefix("needs_ironwood_tool")), () -> Ingredient.of(TwilightGDRegistry.GIANT_IRONWOOD_INGOT.get())), GolemDungeons.loc("ironwood"), List.of(Tiers.IRON), List.of(Tiers.DIAMOND));
 		FIERY = TierSortingRegistry.registerTier(new ForgeTier(4, 8192, 9.0F, 4.0F, 10, BlockTags.create(TwilightForestMod.prefix("needs_fiery_tool")), () -> Ingredient.of(TwilightGDRegistry.GIANT_FIERY_INGOT.get())), GolemDungeons.loc("fiery"), List.of(Tiers.NETHERITE), List.of());
-		KNIGHTMETAL = TierSortingRegistry.registerTier(new ForgeTier(3, 2048, 8.0F, 3.0F, 8, BlockTags.create(TwilightForestMod.prefix("needs_knightmetal_tool")), () -> Ingredient.of(TwilightGDRegistry.GIANT_KNIGHT_INGOT.get())), GolemDungeons.loc("knightmetal"), List.of(Tiers.DIAMOND), List.of(Tiers.NETHERITE));
+		KNIGHTMETAL = TierSortingRegistry.registerTier(new ForgeTier(3, 4096, 8.0F, 3.0F, 8, BlockTags.create(TwilightForestMod.prefix("needs_knightmetal_tool")), () -> Ingredient.of(TwilightGDRegistry.GIANT_KNIGHT_INGOT.get())), GolemDungeons.loc("knightmetal"), List.of(Tiers.DIAMOND), List.of(Tiers.NETHERITE));
 	}
 }
